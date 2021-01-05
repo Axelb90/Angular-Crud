8884061100e6a6741f68cc97ea049cbe69ea79a8
AxL <bellmann.axel90@gmail.com>
2021-01-05 12:45:30 -0300

    login and logout

--- angular-firebase-crud/src/app/shared/navbar/navbar.component.ts
+++ angular-firebase-crud/src/app/shared/navbar/navbar.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  providers: [AuthService]
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
+  public user$: Observable<any> = this.authSvc.afAuth.user;
 
-  constructor() { }
+  constructor(private authSvc: AuthService) { }
 
-  ngOnInit(): void {
+  onLogout(){
+    this.authSvc.logout();
   }
 
 }
